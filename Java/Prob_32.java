dc69b023fd92302e8dd205248dbbb138d4e27079
Akshat Yadav <akshattheta09@gmail.com>
2026-08-17 14:01:29 +0530

    Prob_387_1

--- Java/Prob_32.java
+++ Java/Prob_32.java
@@ -5,35 +5,27 @@
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
 // Every close bracket has a corresponding open bracket of the same type.
-
-import java.util.*
-import java.util.Scanner;
-
-import java.util.*;
+import java.util.Stack;
 
 class Prob_32 {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         
-        for (char brkt : s.toCharArray()) {
-            if (brkt == '(' || brkt == '{' || brkt == '[') {
-                stack.push(brkt);
+        for (char c : s.toCharArray()) {
+            // Push the corresponding closing bracket onto the stack
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
             } else {
-             
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                
-                if ((stack.peek() == '(' && brkt == ')') ||
-                    (stack.peek() == '{' && brkt == '}') ||
-                    (stack.peek() == '[' && brkt == ']')) {
-                    stack.pop();
-                } else {
+                // If the stack is empty or the brackets don't match, it is invalid
+                if (stack.isEmpty() || stack.pop() != c) {
                     return false;
                 }
             }
-        } 
-        
+        }
         return stack.isEmpty();
-    } 
-} 
+    }
+}
--- Java/Prob_387.java
+++ Java/Prob_387.java
@@ -2,3 +2,24 @@
 
 // Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
+import java.util.Stack;
+
+class prob_387{
+    public int firstUniqueChar(String s){
+        int n = s.size();
+        for(int i = 0;i < n;i++){
+            boolean dupFound = false;
+            for(int j =0;j<n ; j++){
+                if(i == j) continue;
+                if(s[i] == s[j]){
+                    dupFound = true;
+                    break;
+                }
+            }
+            if(dupFound == False    ){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
