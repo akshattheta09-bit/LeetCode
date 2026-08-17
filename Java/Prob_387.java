4a4b8776fce2576429f03bce0a31209b4190fb07
Akshat Yadav <akshattheta09@gmail.com>
2026-08-17 14:13:56 +0530

    Prob_387_hashmaps

--- Java/Prob_387.java
+++ Java/Prob_387.java
@@ -23,3 +23,31 @@ class prob_387{
         return -1;
     }
 }
+
+
+
+
+// Solution using HashMap:
+// to reduce the time complexity to O(n) we can use a HashMap to store the frequency of each character in the string. Then, we can iterate through the string again to find the first character with a frequency of 1.
+
+
+Class Solution (
+public int firstUniqChar(String s) {
+    int n = s.length();
+    HashMap<Character, Integer> mp = new HashMap<>();
+
+ for (char c : s.toCharArray()) {
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
+        }
+        
+        char[] str = s.toCharArray();
+
+        for (int i = 0; i < n; i++) {
+            if (mp.get(str[i]) == 1) {
+                return i;
+            }
+        }
+        
+
+        return -1; 
+    }
